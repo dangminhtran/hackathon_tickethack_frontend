2406dfc7deb0e5dc9a7c61cdb67d99d6a53549d1
Dang-Minh TRAN <dang-minh.tran@hotmail.com>
2024-01-31 17:04:52 +0100

    final results

--- bookingpage.js
+++ bookingpage.js
@@ -9,21 +9,24 @@ function Booking() {
             BookingContent.innerHTML = ''
             if (data) {
                 console.log(data.Cart)
-                const timestamp = Date.now()
+            
 
                 for (let i = 0; i < data.Cart.length; i++) {
+                    console.log(data.Cart[i]);
+
                     const date = new Date(data.Cart[i].date).getTime()
                     const hour = String(
                         new Date(data.Cart[i].trip.date).getHours()).padStart(2, "0");
                     const minute = String(
                         new Date(data.Cart[i].trip.date).getMinutes()).padStart(2, "0");
-                        let duration = date - timestamp
+
+                      
                      BookingContent.innerHTML +=
                         `<div class="travelsList" id=${data.Cart[i].trip._id}>
                 <div class="travel">${data.Cart[i].trip.departure} > ${data.Cart[i].trip.arrival}</div>
                 <div class="travel-time">${hour} : ${minute}</div>
                 <div class="travel-price" value='${data.Cart[i].trip.price}'>${data.Cart[i].trip.price}€</div>
-                <div class="time-departure">Departure in ${moment(duration).fromNow()}</div>
+                <div class="time-departure">Departure ${moment(data.Cart[i].trip.date).fromNow()}</div>
                 </div>`
 
             }}})
